f851ec0d1a48ba4b54e5559616da0f476a8f5be2
Shuai Yuan <ishuaiyuan@gmail.com>
2019-10-20 12:09:01 -0400

    first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Shuai Yuan on 10/20/19.
+//  Copyright © 2019 Shuai Yuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
